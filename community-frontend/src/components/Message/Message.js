be7895ff1bef1d392e3103d22d6e96a5bc9996c3
GeorgeC <collingridgegeorge@gmail.com>
2021-03-24 16:50:07 +0000

    chat done

---
+++ community-frontend/src/components/Message/Message.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Message(props) {
+  return (
+    <div style={{ background: "#eee", borderRadius: "5px", padding: "0 10px" }}>
+      <p>
+        <strong>{props.user}</strong> says:
+      </p>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+export default Message;
