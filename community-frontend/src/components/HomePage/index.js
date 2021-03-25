5bd08741a3b8c51a7a454d4197298112cbd7b8b9
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-03-25 12:16:32 +0000

    reactRouter

--- community-frontend/src/components/HomePage/index.js
+++ community-frontend/src/components/HomePage/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import HomePage from '../HomePage/HomePage.css'
+import Input from '../Input/index';
+import Posts from '../Posts/index';
 
 function HomePage() {
   return (
     <div>
+      
+      <Posts/>
+      <Input/>
+
       <h1>HomePage</h1>
     </div>
   );
