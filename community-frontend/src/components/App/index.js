2e65302d9810ec1e96b58442155de5b1b6700a3d
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-03-25 16:30:57 +0000

    manage to use react route

--- community-frontend/src/components/App/index.js
+++ community-frontend/src/components/App/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import Profile from "../Profile";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Profile from "../Profile";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../Login";
 import LogoutButton from "../Logout";
 
-// import Chat from "../Chat/Chat";
+import Chat from "../Chat/Chat";
 import HomePage from "../HomePage/index";
-import NavBar from "../NavBar";
+
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -16,12 +16,48 @@ function App() {
   return (
     <div>
       <div style={{ margin: "0 30%" }}>
-        {!isAuthenticated && <LoginButton />}
-        {isAuthenticated && <LogoutButton />}
-        {isAuthenticated && <HomePage />}
+        {!isAuthenticated && ( 
+          <LoginButton />
+          )
+         }
+        </div>
+
+        {isAuthenticated && (
+      <div>
+    <LogoutButton />
+    <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/chat">Chat</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/chat">
+            <Chat />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>  
+      </Router>
       </div>
-      <NavBar />
+        )
+          }
+    
+     
     </div>
+   
   );
 }
 
--- community-frontend/src/components/NavBar/index.js
+++ community-frontend/src/components/NavBar/index.js
@@ -1,40 +1,12 @@
 import "./NavBar.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Profile from "../Profile";
-import Chat from "../Chat/Chat";
-import HomePage from "../HomePage";
+
 
 function NavBar() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/chat">Chat</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/chat">
-            <Chat />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+  return(
+    <>
+
+    </>
+  )
 }
 
 export default NavBar;
