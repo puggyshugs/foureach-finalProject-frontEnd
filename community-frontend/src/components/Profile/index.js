3c38479b7514b4af6f6dc36a032f5b520630f748
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-03-30 08:49:29 +0100

    some css for profile component

--- community-frontend/src/components/Profile/index.js
+++ community-frontend/src/components/Profile/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import css from './Profile.module.css';
 
 const Profile = () => {
   const { loading, user } = useAuth0();
@@ -9,13 +12,19 @@ const Profile = () => {
   }
 
   return (
-    <>
-      <img src={user.picture} alt="Profile Picture" />
-
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      <code>{JSON.stringify(user, null, 2)}</code>
-    </>
+    <Card className={css.container} style={{ width: "30rem" }}>
+      <Card.Img className={css.image} variant="top" src={user.picture} alt="Profile Picture" />
+      <Card.Body>
+        <Card.Title>
+          <h2>{user.name}</h2>
+        </Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
