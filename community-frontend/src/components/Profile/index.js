5a27b539c472af92af9ee07c68003bb704e6d7e8
GeorgeC <collingridgegeorge@gmail.com>
2021-04-02 16:27:56 +0100

    logo added home page

--- community-frontend/src/components/Profile/index.js
+++ community-frontend/src/components/Profile/index.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import css from "./Profile.module.css";
 import { MDBInput } from "mdbreact";
@@ -6,37 +6,39 @@ import { StatHelpText } from "@chakra-ui/stat";
 
 const Profile = () => {
   const { loading, user } = useAuth0();
-  const[bio, setBio] = useState('');
-  const[text, setText] = useState('');
-   
+  const [bio, setBio] = useState("");
+  const [text, setText] = useState("");
+
   if (loading || !user) {
     return <div>Loading...</div>;
   }
-  function updateBio(e){
-    setBio(e.target.value)
+  function updateBio(e) {
+    setBio(e.target.value);
   }
-   
-  function addToBio(){
 
-    setText(bio)
+  function addToBio() {
+    setText(bio);
   }
-  
 
   return (
     <div className={css.profilePageContainer}>
-    <div className={css.profileInfoContainer}>
-      <img className={css.image} src={user.picture} alt="profile"></img>
-      <h2 className={css.profileName}>{user.name}</h2>
-      <MDBInput
-        className={css.inputField}
-        type="textarea"
-        label="Type notification..."
-        rows="2"
-        icon="pencil-alt"
-        onChange={(e) => updateBio(e)}
-      />
-      <button onClick={() => addToBio(text)}>Add bio</button>
-      <p className={css.bioText}>{text}</p>
+      <div className={css.profileInfoContainer}>
+        <img className={css.image} src={user.picture} alt="profile"></img>
+        <h2 className={css.profileName}>{user.name}</h2>
+        <MDBInput
+          className={css.inputField}
+          type="textarea"
+          label="Type notification..."
+          rows="2"
+          icon="paint-brush"
+          onChange={(e) => updateBio(e)}
+        />
+        <button className={css.addBioButton} onClick={() => addToBio(text)}>
+          Add bio
+        </button>
+        <div className={css.bioTextBox}>
+          <p className={css.bioText}>{text}</p>
+        </div>
       </div>
     </div>
   );
